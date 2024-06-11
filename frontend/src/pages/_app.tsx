96baab6544da54a2f325b1341842b2421022c96b
Lucky Victory <victorylucky96@gmail.com>
2024-06-11 06:23:06 +0000

    updated package

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -9,7 +9,6 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
